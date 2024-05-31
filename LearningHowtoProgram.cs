bcda84f18f9f40787e4cc64004564fce20091d44
Annan Cahya <97139161+annancahya@users.noreply.github.com>
2024-05-31 20:56:26 +0800

    Create LearningHowtoProgram.cs
    Contoh variables yang sering digunakan saat menggunakan C# di Unity,

---
+++ LearningHowtoProgram.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LearningHowtoProgram : MonoBehaviour
+{
+    // sblm deklrasi variabel harus ada type-nya dl
+    float speed = 5.0f;
+
+    double mana = 15.5;
+
+    int health = 100;
+
+    string playerName = "Warrior";
+
+    bool isDead = false;
+
+    char oneChar = 'A';
+
+    /* Mencoba cara comment di c# 
+       ini tidak bakal ke compile
+       Jadi bagus untuk note yg penting
+     */
+
+} //class
